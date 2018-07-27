399a3229dc1cd5d8d6f66eec5e4cae73e0e065d3
Avickerman <Avickerm@gmail.com>
2018-07-26 23:42:14 -0500

    commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burgers = require("../models/burgers.js");
+
+router.get('/', function (req, res) 
+{
+  res.redirect('/index');
+});
+
+// Index Page 
+router.get('/index', function (req, res) 
+{
+  burger.selectAll(function(data) 
+  {
+    var hbsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+
+router.post('/burger/create', function (req, res) 
+{
+  burger.insertOne(req.body.burger_name, function() 
+  {
+    res.redirect('/index');
+  });
+});
+
+router.post('/burger/eat/:id', function (req, res) 
+{
+  burger.updateOne(req.params.id, function() 
+  {
+    res.redirect('/index');
+  });
+});
+
+  
+  // Export routes for server.js to use.
+  module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Mushroom Burger', false);
